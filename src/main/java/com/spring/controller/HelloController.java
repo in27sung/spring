348ea6a19c7a89a6df390a5be1503ee5bedb466c
insung <in27sung@gmail.com>
2025-01-04 21:46:10 +0900

    Initial commit

---
+++ src/main/java/com/spring/controller/HelloController.java
@@ -0,0 +1,17 @@
+package com.spring.controller;
+
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
+public class HelloController {
+
+    @GetMapping("hello")
+    public String hello(Model model){
+
+        model.addAttribute("data","인성님!");
+
+        return "hello";
+    }
+}
