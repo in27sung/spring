7f25d6cf66493bc79fc5aefcfb71592bb6bff10c
insung <in27sung@gmail.com>
2025-01-05 19:28:53 +0900

    Add MVC pattern

--- src/main/java/com/spring/controller/HelloController.java
+++ src/main/java/com/spring/controller/HelloController.java
@@ -3,6 +3,7 @@ package com.spring.controller;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 
 @Controller
 public class HelloController {
@@ -13,5 +14,12 @@ public class HelloController {
         model.addAttribute("data","인성님!");
 
         return "hello";
+    }
+
+    @GetMapping("hello-mvc")
+    public String helloMvc(@RequestParam(value = "name") String name, Model model){
+        model.addAttribute("name", name);
+
+        return "hello-template";
     }
 }
